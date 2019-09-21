773b72838155727b0d2089d8771fe899167b6c46
Ben Grabowski <gbowski30@gmail.com>
2019-09-20 20:13:16 -0600

    server is working correctly

---
+++ server.js
@@ -0,0 +1,53 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
+const app = express()
+
+app.use(morgan('common'))
+app.use(helmet())
+app.use(cors())
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({error: 'Unauthorized request'})
+    }
+    next()
+})
+
+const MOVIES = require('./movie-data.json')
+
+app.get('/movie', (req, res) => {
+    const { genre, country, avg_vote } = req.query;
+
+    let response = MOVIES;
+
+    if(genre) {
+        response = response.filter(movie => {
+            return movie.genre.toLowerCase().includes(genre.toLowerCase())
+        })
+    }
+
+    if(country) {
+        response = response.filter(movie => {
+            return movie.country.toLowerCase().includes(country.toLowerCase())
+        })
+    }
+
+    if(avg_vote) {
+        response = response.filter(movie => {
+            return movie.avg_vote >= Number(avg_vote)
+        })
+    }
+    res.json(response)
+})
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`)
+})
